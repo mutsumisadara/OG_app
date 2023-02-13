d1f2005d66bf6fbdfd4e796159b304777fc0aea0
mutsumisadara <mutsumisadara@gmail.com>
2023-02-13 16:04:33 +0900

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'animal_managements/new'
   devise_for :users
   # root to: 'users#sign_up'
   resources :users
   resources :groups
   resources :animals
+  resources :animal_managements
 end
