83d04f3b3a242486de266db5c17375b99fe51abc
mutsumisadara <mutsumisadara@gmail.com>
2023-02-15 16:01:57 +0900

    add simple_calender view and design

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,40 @@
 class SchedulesController < ApplicationController
+  
   def index
+    @schedules = Schedule.all
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    if @schedule.save(schedule_params)
+      redirect_to schedules_path, notice: 'スケジュールを追加しました'
+    else
+      redirect_to schedules_path, notice: '失敗しました'
+    end
+  end
+
+  def show
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:id, :content, :start_time)
   end
 end
