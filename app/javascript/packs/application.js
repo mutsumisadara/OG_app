2927a185b4b370bdc1f092c6eb8593365ee5c35a
mutsumisadara <mutsumisadara@gmail.com>
2023-02-24 16:43:07 +0900

    update logout function from admin screen

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,6 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import 'bootstrap';
-import '../stylesheets/application';
 import "channels"
 
 Rails.start()
