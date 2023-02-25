a7f60486db22a636095c61e1eee6b3addc998dc3
mutsumisadara <mutsumisadara@gmail.com>
2023-02-25 10:00:57 +0900

    add animals validations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,6 +3,6 @@ class Animal < ApplicationRecord
     belongs_to :group
     has_many :animal_managements , dependent: :destroy
     # accepts_nested_attributes_for :animal_managements #追記
-    validates :name, presence: true
-    validates :sex, inclusion: [true, false]
+    validates :name, :birthday, presence: true
+    validates :sex, inclusion: { in: [true, false], allow_nil: true }
 end
